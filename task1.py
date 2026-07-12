2137488c924284274f8bd63054146c3f3a87ed60
omezmr_work_PC <Omezovmr@argtmn.ru>
2026-07-12 19:51:58 +0500

    Расскоментированы проверки по 1, 2 и 3 заданию (к уже имеющейся проверки по 4-му заданию

--- task1.py
+++ task1.py
@@ -94,41 +94,41 @@ class Reviewer(Mentor):
 
 ##ПРОВЕРКА ПЕРВОЙ ЧАСТИ
 
-# lecturer = Lecturer('Иван', 'Иванов')
-# reviewer = Reviewer('Пётр', 'Петров')
-# print(isinstance(lecturer, Mentor)) # True
-# print(isinstance(reviewer, Mentor)) # True
-# print(lecturer.courses_attached)    # []
-# print(reviewer.courses_attached)    # []
+lecturer = Lecturer('Иван', 'Иванов')
+reviewer = Reviewer('Пётр', 'Петров')
+print(isinstance(lecturer, Mentor)) # True
+print(isinstance(reviewer, Mentor)) # True
+print(lecturer.courses_attached)    # []
+print(reviewer.courses_attached)    # []
 
 ##ПРОВЕРКА ВТОРОЙ ЧАСТИ
 
-# lecturer = Lecturer('Иван', 'Иванов')
-# reviewer = Reviewer('Пётр', 'Петров')
-# student = Student('Алёхина', 'Ольга', 'Ж')
-#
-# student.courses_in_progress += ['Python', 'Java']
-# lecturer.courses_attached += ['Python', 'C++']
-# reviewer.courses_attached += ['Python', 'C++']
-#
-# print(student.rate_lecture(lecturer, 'Python', 7))  # None
-# print(student.rate_lecture(lecturer, 'Java', 8))  # Ошибка
-# print(student.rate_lecture(lecturer, 'С++', 8))  # Ошибка
-# print(student.rate_lecture(reviewer, 'Python', 6))  # Ошибка
-#
-# print(lecturer.grades)  # {'Python': [7]}
+lecturer = Lecturer('Иван', 'Иванов')
+reviewer = Reviewer('Пётр', 'Петров')
+student = Student('Алёхина', 'Ольга', 'Ж')
+
+student.courses_in_progress += ['Python', 'Java']
+lecturer.courses_attached += ['Python', 'C++']
+reviewer.courses_attached += ['Python', 'C++']
+
+print(student.rate_lecture(lecturer, 'Python', 7))  # None
+print(student.rate_lecture(lecturer, 'Java', 8))  # Ошибка
+print(student.rate_lecture(lecturer, 'С++', 8))  # Ошибка
+print(student.rate_lecture(reviewer, 'Python', 6))  # Ошибка
+
+print(lecturer.grades)  # {'Python': [7]}
 
 ##ПРОВЕРКА ТРЕТЬЕЙ ЧАСТИ
 
-# print(reviewer)
+print(reviewer)
 
-# lecturer.grades = {'Python': [2, 3, 4, 5, 5, 4, 3, 2, 1]}
-# print(lecturer)
-#
-# student.grades = {'Python': [2, 3, 4, 5, 5, 4, 3, 2, 1]}
-# student.courses_in_progress = ['Java', 'C++']
-# student.finished_courses = ['AI']
-# print(student)
+lecturer.grades = {'Python': [2, 3, 4, 5, 5, 4, 3, 2, 1]}
+print(lecturer)
+
+student.grades = {'Python': [2, 3, 4, 5, 5, 4, 3, 2, 1]}
+student.courses_in_progress = ['Java', 'C++']
+student.finished_courses = ['AI']
+print(student)
 
 ##ИТОГОВАЯ ПРОВЕРКА (4 ЗАДАНИЕ)
 
